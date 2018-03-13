6895519b964b802c1393401328fb8d4c0d73faec
Aslan <E_Aslan94@hotmail.com>
2018-03-13 10:52:18 +0100

    Log in placeholders toegevoegd

--- LaRustique/LaRustique/Form1.Designer.cs
+++ LaRustique/LaRustique/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.aButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 24);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(181, 20);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Enter += new System.EventHandler(this.txtUsername_Enter);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUsername_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 75);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // aButton
+            // 
+            this.aButton.Location = new System.Drawing.Point(57, 137);
+            this.aButton.Name = "aButton";
+            this.aButton.Size = new System.Drawing.Size(75, 25);
+            this.aButton.TabIndex = 0;
+            this.aButton.Text = "Log in";
+            this.aButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(205, 199);
+            this.Controls.Add(this.aButton);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "Form1";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button aButton;
     }
 }
 
--- LaRustique/LaRustique/Form1.cs
+++ LaRustique/LaRustique/Form1.cs
@@ -15,6 +15,61 @@ namespace LaRustique
         public Form1()
         {
             InitializeComponent();
+            //Zet de placeholders van gebruikersnaam en wachtwoord
+            txtUsername.Font = new Font(txtUsername.Font, FontStyle.Italic);
+            txtUsername.ForeColor = Color.Gray;
+            txtUsername.Text = "Gebruikersnaam";
+
+            //Wachtwoord
+            txtPassword.Font = new Font(txtPassword.Font, FontStyle.Italic);
+            txtPassword.ForeColor = Color.Gray;
+            txtPassword.Text = "Wachtwoord";
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            //Verwijder placeholder als txtbox geselecteerd is
+            if (txtUsername.Text == "Gebruikersnaam")
+            {
+                txtUsername.Clear();
+                txtUsername.ForeColor = Color.Black;
+                txtUsername.Font = new Font(txtUsername.Font, FontStyle.Regular);
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            //Als de tekst leeg is word de placeholder teruggezet
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Font = new Font(txtUsername.Font, FontStyle.Italic);
+                txtUsername.ForeColor = Color.Gray;
+                txtUsername.Text = "Gebruikersnaam";
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            //Verwijder placeholder als txtbox geselecteerd is
+            if (txtPassword.Text == "Wachtwoord")
+            {
+                txtPassword.Clear();
+                txtPassword.Font = new Font(txtPassword.Font, FontStyle.Regular);
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            //Als de tekst leeg is word de placeholder teruggezet
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Font = new Font(txtPassword.Font, FontStyle.Italic);
+                txtPassword.ForeColor = Color.Gray;
+                txtPassword.UseSystemPasswordChar = false;
+                txtPassword.Text = "Wachtwoord";
+            }
         }
     }
 }
